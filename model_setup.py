8b9f6551107e4a1669ec0f69e410bb58ea1cd7d6
gwjm <gregor@the-macdonalds.net>
2024-04-27 13:32:11 -0700

    Fixed Question Generation Bugs

--- model_setup.py
+++ model_setup.py
@@ -1,5 +1,5 @@
 import torch
-from transformers import pipeline, BitsAndBytesConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer,  BitsAndBytesConfig
 from huggingface_hub import login
 from constants import AUTH_TOKEN, PROMPT_FORMAT_STRING
 
@@ -7,13 +7,24 @@ def model_setup():
     model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
     login(token=AUTH_TOKEN)
 
-    config = BitsAndBytesConfig(load_in_8bit=True)
-
-    model_pipeline = pipeline(
-        task="text-generation",
-        model=model_id,
-        model_kwargs={"quantization_config": config, "trust_remote_code": True, "torch_dtype": torch.qint8},
-        device_map="auto"
+    config = BitsAndBytesConfig(
+        load_in_8bit = True,
+        llm_int8_threshold = 20.0,
+        llm_int8_enable_fp32_cpu_offload = False,
+        llm_int8_has_fp16_weight = False
     )
 
-    return model_pipeline
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_id, trust_remote_code=True, padding=True, padding_side="left"
+    )
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        quantization_config=config,
+        device_map="auto",
+        trust_remote_code=True
+    )
+
+
+    return model, tokenizer

1d5983bd05bd3d4ce960bf7eeb4702bab7bd816d
jacobbia@comcast.net <jacobbia@comcast.net>
2024-04-27 13:52:46 -0700

    cleaned jacob's method for eval student

--- evaluate_student.py
+++ evaluate_student.py
@@ -24,24 +24,20 @@ def evaluate_student_by_attempts_til_correct(df, student_id):
 
     # sorry for ugly code :( all good lol
 
-    df["correct"] = (df["points_earned"] == df["points_possible"])
-    topic_counts = df.groupby("student_id")["page_topic"].nunique()
-    bad_studs = topic_counts[topic_counts < 10].index
-    good_studs = df[~(df["student_id"].isin(bad_studs))].reset_index(drop=True)
-    good_studs_correct = good_studs[good_studs["correct"]]
-    good_studs_correct = good_studs_correct[["student_id", "book", "page_topic", "item_id", "prompt", "attempt"]]
-    min_attempts = good_studs_correct.groupby(["student_id", "item_id", "prompt", "page_topic"])["attempt"].min()
-    student_average_attempts_by_topic = min_attempts.reset_index().groupby(["student_id", "page_topic"])["attempt"].mean().reset_index()
-    student_data = student_average_attempts_by_topic[student_average_attempts_by_topic['student_id'] == student_id]
-    topics_list = list(student_data.sort_values(by="attempt", ascending=False)["page_topic"].iloc[0:3])
-    book = df[df['student_id'] == student_id]["book"].unique()[0]
-    if (book == "College / Advanced Statistics and Data Science (ABCD)"):
-        level = "advanced college"
-    elif (book == "College / Statistics and Data Science (ABC)"):
-        level = "college"
-    else:
-        level = "high school"
-    return topics_list, level
+    stud = df[df["student_id"] == student_id]
+
+    if (stud["page_topic"].nunique() < 10):
+        print("Student Must Answer More Questions")
+        return None
+
+    stud["correct"] = (df["points_earned"] == df["points_possible"])
+    correct_questions = stud[stud["correct"]]
+    correct_questions = correct_questions[[["page_topic", "item_id", "prompt", "attempt"]]]
+
+    min_attempts = correct_questions.groupby(["item_id", "prompt", "page_topic"])["attempt"].min()
+    average_attempts_by_topic = min_attempts.reset_index().groupby(["page_topic"])["attempt"].mean().reset_index()
+    topics_list = list(average_attempts_by_topic.sort_values(by="attempt", ascending=False)["page_topic"].iloc[0:3])
+    return topics_list
 
 def evaluate_student_wrapper(studetnt_id):
     '''
@@ -53,5 +49,13 @@ def evaluate_student_wrapper(studetnt_id):
 
 
     df = pd.read_csv("Data/Full Data/scores.csv")
+
+    book = df[df['student_id'] == student_id]["book"].unique()[0]
+    if (book == "College / Advanced Statistics and Data Science (ABCD)"):
+        level = "advanced college"
+    elif (book == "College / Statistics and Data Science (ABC)"):
+        level = "college"
+    else:
+        level = "high school"
 
     #TODO Pass the Df and the student_id to the function to get the topics and the level

a739755952c8152ec95ef07173f10813c4b5edca
gwjm <gregor@the-macdonalds.net>
2024-04-27 00:15:09 -0700

    added argparsing to evaluate_student.py

--- evaluate_student.py
+++ evaluate_student.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import argparse
 
 def evaluate_student(student_id):
     '''
@@ -23,6 +24,14 @@ def main():
     :return:
     '''
     # We will assume that the CSVs are in the Data folder
+    #add argparse to get the student id and the evaluation method
+    parser = argparse.ArgumentParser()
+    parser.add_argument("student_id", help="The student ID to evaluate")
+    parser.add_argument("evaluation_method", help="The method to evaluate the student")
+    args = parser.parse_args()
+    student_id = args.student_id
+    evaluation_method = args.evaluation_method
+
     df = pd.read_csv("Data/Full Data/updated_topics.csv")
 
     #TODO: Implement the functions to evaluate students using various heuristics
